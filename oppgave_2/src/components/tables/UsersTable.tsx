84eafd245ccb9281de7e4e40ba57d4b77e7d8aed
Mohammed Ghazwan Almilhim <112077465+mohammga@users.noreply.github.com>
2023-11-26 21:49:39 +0100

    update

--- oppgave_2/src/components/tables/UsersTable.tsx
+++ oppgave_2/src/components/tables/UsersTable.tsx
@@ -1,12 +1,36 @@
-import type { User } from "@/types"
+"use client";
+
+import { useState } from 'react';
+import type { User } from "@/types";
 
 interface UsersTableProps {
-  users: User[]
+  users: User[];
 }
 
 const UsersTable: React.FC<UsersTableProps> = ({ users }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const filteredUsers = users.filter((user) => {
+    return (
+      user.id.toString().includes(searchTerm) ||
+      user.userId.toString().includes(searchTerm)
+    );
+  });
+
   return (
-      <section className="">
+    <section className="p-6">
+      <div className="mb-4">
+        <input
+          type="text"
+          id="search"
+          placeholder="Søk etter utøvere"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      {filteredUsers.length === 0 ? (
+        <p>Ingen utøvere funnet.</p>
+      ) : (
         <table className="mt-4 w-full border-collapse">
           <thead>
             <tr>
@@ -19,7 +43,7 @@ const UsersTable: React.FC<UsersTableProps> = ({ users }) => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {filteredUsers.map((user) => (
               <tr key={user.id}>
                 <td className="border px-4 py-2">{user.id}</td>
                 <td className="border px-4 py-2">{user.userId}</td>
@@ -36,8 +60,9 @@ const UsersTable: React.FC<UsersTableProps> = ({ users }) => {
             ))}
           </tbody>
         </table>
-      </section>
-  )
-}
+      )}
+    </section>
+  );
+};
 
-export default UsersTable
+export default UsersTable;
