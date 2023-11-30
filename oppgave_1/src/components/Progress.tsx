179dde5f789e054bca64ad3d53be3198bf93d549
Mohammed Ghazwan Almilhim <mohammga@itstud.hiof.no>
2023-11-30 17:03:14 +0100

    update

--- oppgave_1/src/components/Progress.tsx
+++ oppgave_1/src/components/Progress.tsx
@@ -1,25 +1,19 @@
 "use client"
 
 type TasksProps = {
-  next: () => void;
-  previous: () => void;
-  current: number;
+  next: () => void
 }
 
-export default function Progress({ next, previous, current}: TasksProps) {
-
+export default function Progress({ next }: TasksProps) {
   return (
-<footer className="mt-3 space-y-4">
-  <button onClick={next} type="button" className="bg-black rounded-sm text-white w-full py-2">
-    Neste
-  </button>
-
-  {current !== 0 && (
-    <button onClick={previous} type="button" className="bg-black rounded-sm text-white w-full py-2">
-      Forrige
-    </button>
-  )}
-</footer>
-
+    <footer className="mt-3 space-y-4">
+      <button
+        onClick={next}
+        type="button"
+        className="w-full rounded-sm bg-black py-2 text-white"
+      >
+        Neste
+      </button>
+    </footer>
   )
 }
